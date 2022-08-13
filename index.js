0e50d30859385407038df5b2b0275ab1c4bad30e
meskyA <mesky2015@gmail.com>
2022-08-13 13:06:25 -0700

    adds package.json and adds codes to run node

--- index.js
+++ index.js
@@ -1,13 +1,128 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+// const questions = [];
+const questions = () =>
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "author",
+            message: "What is the author's name?"
+        },
+        {
+            type: "input",
+            name: "username",
+            message: "What is your GitHub username?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is your email address?"
+        },
+        {
+            type: "input",
+            name: "title",
+            message: "What is your project title?"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Write a brief description of your project:"
+        },
+        {
+            type: "input",
+            name: "author",
+            message: "What is the author's name?"
+        },
+        {
+            type: "list",
+            name: "license",
+            message: "What kind of license does your project have?",
+            choices: ["MIT", "APACHE 2.0", "BSD 3 Clause", "BSD 2 Clause", "Boost 1.0", "None"]
+        },
+        {
+            type: "input",
+            name: "installation",
+            message: "What command should be used to run to install dependencies?"
+        },
+        {
+            type: "input",
+            name: "tests",
+            message: "What commands should be used to run tests?"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "What does the user need to know to use the repo?"
+        },
+        {
+            type: "input",
+            name: "contributing",
+            message: "What guidelines should the user know about contributing to the repo??"
+        },
+    ]);
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+function generateReadme(data){
+    return`
+        #${data.title}
+        ${badge}
+        ${data.desctiption}
+
+        ## Table of Contents
+
+        - [Project Description](#Description)
+        - [Installation](#Installation)
+        - [Usage] (#Usage)
+        - [Contribution](#Contribution)
+        - [test](#test)     
+        - [Questions](#Questions])
+        - [License](#License)
+
+        ### Description
+        ${data.desctiption}
+
+        ### Installation
+        In order to install the necessary dependencies, open the console and run the following:
+        \`\`\`${data.installation}\`\`\`
+        
+
+        ### Usage
+        ${data.usage}  
+
+        ### Contributing
+        ${data.contributing}
+
+        ### Test
+        In order to test, open the console and run the following
+        \`\`\`${data.test}\`\`\`
+
+        ## Questions
+        Should you have any questions about regarding this project, please contact ${data.author} on [GitHub](https://github.com/ ${data.username} or at
+        ${data.email}
+       
+
+        ## License
+        The project is licensed under:
+        ${data.license}
+
+        `   
+   
+}
 
 // TODO: Create a function to initialize app
 function init() {}
+questions()
+.then((data) => writeFileAsync('generatedREADME.md', generateMD(data)))
+.then(() => console.log('Sucessfully wrote to index.html'))
+.catch((err) => console.error(err));
 
 // Function call to initialize app
 init();
--- library/ReadmeGen.js
+++ library/ReadmeGen.js
@@ -1,42 +1,84 @@
-class Markdown {
-    static generateReadme(answers){
-        return`
-        #${answers.title}
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// const util = require('util');
 
-        ### Table of Contents
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-        - [Project Description](#Description)
-        - [Installation](#Installation)
-        - [Usage] (#Usage)
-        - [Contribution](#Contribution)
-        - [test](#test)     
-        - [Questions](#Questions])
-        - [License](#License)
+// const questions = () =>
+//     inquirer.prompt([
+//         {
+//             type: "input",
+//             name: "author",
+//             message: "What is the author's name?"
+//         },
+//         {
+//             type: "input",
+//             name: "username",
+//             message: "What is your GitHub username?"
+//         },
+//         {
+//             type: "input",
+//             name: "email",
+//             message: "What is your email address?"
+//         },
+//         {
+//             type: "input",
+//             name: "title",
+//             message: "What is your project title?"
+//         },
+//         {
+//             type: "input",
+//             name: "author",
+//             message: "What is the author's name?"
+//         },
+//     ])
+// class Markdown {
+//     static generateReadme(answers){
+//         return`
+//         #${answers.title}
 
-        ## Description
-        ${answers.desctiption}
+//         ### Table of Contents
 
-        ## Installation
-        ${answers.installation}
+//         - [Project Description](#Description)
+//         - [Installation](#Installation)
+//         - [Usage] (#Usage)
+//         - [Contribution](#Contribution)
+//         - [test](#test)     
+//         - [Questions](#Questions])
+//         - [License](#License)
 
-        ## Usage
-        ${answers.usage}  
+//         ## Description
+//         ${answers.desctiption}
 
-        ## Contribution
-        ${answers.contribution}
+//         ## Installation
+//         ${answers.installation}
 
-        ## Test
-        ${answers.test}
+//         ## Usage
+//         ${answers.usage}  
 
-        ## Questions
-        ${answers.email}
-        ${answers.gitHub}
+//         ## Contribution
+//         ${answers.contribution}
 
-        ## License
-        ${answers.license}
+//         ## Test
+//         ${answers.test}
 
-        `   
-    }
-}
-module.exports = Markdown
-// description, installation instructions, usage information, contribution guidelines, and test instructions
+//         ## Questions
+//         ${answers.email}
+//         ${answers.gitHub}
+
+//         ## License
+//         ${answers.license}
+
+//         `   
+//     }
+// }
+// module.exports = Markdown
+// // description, installation instructions, usage information, contribution guidelines, and test instructions
+// // The Markdown is referring to the utils\generateMarkdown.js file that you will need to require inside of the index.js file at the top
+
+
+
+
+
+// // 10:29
+// // I believe you need to require fs, path, inquirer, and the generateMarkdown js file from the utils folder
